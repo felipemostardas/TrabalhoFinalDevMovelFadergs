2f40d275c7cf330a86333489c448a779f2562621
kazaque <felipemostardas@gmail.com>
2019-05-29 21:38:21 -0300

    implementado as fotos dos anuncios

--- app/src/main/java/com/example/trabalhofinaldevmovelfadergs/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/example/trabalhofinaldevmovelfadergs/activity/CadastrarAnuncioActivity.java
@@ -1,8 +1,12 @@
 package com.example.trabalhofinaldevmovelfadergs.activity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -10,22 +14,29 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.blackcat.currencyedittext.CurrencyEditText;
 import com.example.trabalhofinaldevmovelfadergs.R;
 import com.example.trabalhofinaldevmovelfadergs.helper.Permissoes;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
-public class CadastrarAnuncioActivity extends AppCompatActivity {
+public class CadastrarAnuncioActivity extends AppCompatActivity
+            implements View.OnClickListener {
 
     private EditText campoTitulo, campoDescricao;
+    private ImageView imagem1, imagem2, imagem3;
     private CurrencyEditText campoValor;
     private EditText campoTelefone;
 
     private String[] permissoes = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE
     };
+
+    private List<String> listaFotosRecuperadas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +56,69 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.imageCadastro1 :
+                escolherImagem(1);
+                break;
+
+            case R.id.imageCadastro2 :
+                escolherImagem(2);
+                break;
+
+            case R.id.imageCadastro3 :
+                escolherImagem(3);
+                break;
+        }
+
+    }
+
+    public void escolherImagem(int requestCode){
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, requestCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK){
+
+            //recuperar imagem
+            Uri imagemSelecionada = data.getData();
+            String caminhoImagem = imagemSelecionada.toString();
+
+            // configurar imagem no imageview
+            if(requestCode == 1){
+                imagem1.setImageURI(imagemSelecionada);
+                listaFotosRecuperadas.add(caminhoImagem);
+            }else if(requestCode == 2){
+                imagem2.setImageURI(imagemSelecionada);
+            }else if(requestCode == 3){
+                imagem3.setImageURI(imagemSelecionada);
+            }
+
+            listaFotosRecuperadas.add(caminhoImagem);
+
+
+        }
+    }
+
     private void inicializarComponentes(){
 
         campoTitulo = findViewById(R.id.editTitulo);
         campoDescricao = findViewById(R.id.editDescricao);
         campoValor = findViewById(R.id.editValor);
         campoTelefone = findViewById(R.id.editTelefone);
+        imagem1 = findViewById(R.id.imageCadastro1);
+        imagem2 = findViewById(R.id.imageCadastro2);
+        imagem3 = findViewById(R.id.imageCadastro3);
+        imagem1.setOnClickListener(this);
+        imagem2.setOnClickListener(this);
+        imagem3.setOnClickListener(this);
 
         //configurar portugues brasil para aparecei reais na mascara da moeda
         Locale locale = new Locale("pt", "BR");
